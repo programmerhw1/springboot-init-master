85048b55140aec279b668d55e36376477ba0aecc
kaka <1823188198@qq.com>
2026-05-24 02:43:06 +0800

    feat:功能完善
    完善

---
+++ src/main/java/com/yuan/mianshi/mapper/QuestionMapper.java
@@ -0,0 +1,18 @@
+package com.yuan.mianshi.mapper;
+
+import com.yuan.mianshi.model.entity.Question;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author macbook
+* @description 针对表【question(题目)】的数据库操作Mapper
+* @createDate 2026-05-23 23:05:21
+* @Entity com.yuan.mianshi.model.entity.Question
+*/
+public interface QuestionMapper extends BaseMapper<Question> {
+
+}
+
+
+
+
