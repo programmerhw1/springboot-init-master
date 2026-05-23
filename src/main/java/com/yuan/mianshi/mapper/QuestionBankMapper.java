2f0ed83d188618d121455c0410c4415cfc95d07b
kaka <1823188198@qq.com>
2026-05-24 02:43:06 +0800

    feat:功能完善

---
+++ src/main/java/com/yuan/mianshi/mapper/QuestionBankMapper.java
@@ -0,0 +1,18 @@
+package com.yuan.mianshi.mapper;
+
+import com.yuan.mianshi.model.entity.QuestionBank;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author macbook
+* @description 针对表【question_bank(题库)】的数据库操作Mapper
+* @createDate 2026-05-23 23:05:21
+* @Entity com.yuan.mianshi.model.entity.QuestionBank
+*/
+public interface QuestionBankMapper extends BaseMapper<QuestionBank> {
+
+}
+
+
+
+
---
+++ src/main/java/com/yuan/mianshi/mapper/QuestionBankQuestionMapper.java
@@ -0,0 +1,18 @@
+package com.yuan.mianshi.mapper;
+
+import com.yuan.mianshi.model.entity.QuestionBankQuestion;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author macbook
+* @description 针对表【question_bank_question(题库题目)】的数据库操作Mapper
+* @createDate 2026-05-23 23:05:21
+* @Entity com.yuan.mianshi.model.entity.QuestionBankQuestion
+*/
+public interface QuestionBankQuestionMapper extends BaseMapper<QuestionBankQuestion> {
+
+}
+
+
+
+
